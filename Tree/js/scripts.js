5c03becdd7fef51b5eee2c55e5f26f4fb692483a
Matere29 <125877628+Matere29@users.noreply.github.com>
2023-04-03 14:58:28 +0200

    Add files via upload

---
+++ Tree/js/scripts.js
@@ -0,0 +1,15 @@
+let x = '*'
+
+const y = x + x
+const z = x + y
+const w = x + z
+const h = x + w
+
+console.log("" + x + "")
+console.log(y)
+console.log(z)
+console.log(w)
+console.log(h)
+console.log(" " + x +" ") 
+console.log(" " + x +" ")
+console.log(h) 
