8719f7c8f48f27f03416d25521855e725ce0a4ff
Matere29 <125877628+Matere29@users.noreply.github.com>
2023-04-09 17:27:32 +0200

    Add files via upload

--- FizzBuzz/Js/Scripts.js
+++ FizzBuzz/Js/Scripts.js
@@ -1,14 +1,15 @@
 
-let number = 100;
+let number = 1;
 
 for(number=1;number<=100;number++){
     console.log(number);
-    if(number%3==0){
-        console.log("Fizz");
-    }else if(number%5==0){
-        console.log("Buzz");
-    }else if((number%3 && number%5)==0){
+    number++
+    if(number%3===0 && number%5===0){
         console.log("FizzBuzz");
+    }else if(number%3===0){
+        console.log("Fizz");
+    }else if(number%5===0 ){
+        console.log("Buzz");
     }
 
 
